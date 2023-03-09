7b7c4abee7775c79f289a570b3215ab679079237
TheBerlatto <eduardoberlatto@gmail.com>
2023-03-09 14:19:15 -0300

    criando o arquivo de exemplo operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Eduardo Berlatto";
+        
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final double PI = 3.14;
     }
 }
