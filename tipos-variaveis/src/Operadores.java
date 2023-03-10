724e972a24785cc16fcf378217a8747010cffef9
TheBerlatto <eduardoberlatto@gmail.com>
2023-03-10 12:20:03 -0300

    finalização da classe operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,21 +1,49 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        String concatenacao = "?";
-
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
-
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
+        //positivo e negativo
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);
         
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        numero = numero * -1;
+        System.out.println(numero);
+
+        //incremento e decremento
+        numero = 10;
+        System.out.println(numero++);
+        System.out.println(numero);
+
+        numero = 5;
+        System.out.println(numero--);
+        System.out.println(numero);
+
+        //negação
+        boolean variavel = true;
+        System.out.println(!variavel);
+        System.out.println(variavel);
+
+        //operador ternário
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "os valores são iguais" : "os valores são diferentes";
+        System.out.println(resultado);
+
+        //condicional
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras!");
+        } 
+
+        if(condicao1 || condicao2){
+            System.out.println("Uma das condições é verdadeira!");
+        } 
+
+        System.out.println("Fim!");
     }
 }
